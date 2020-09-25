897328bd03149b14dc499ed0dcf6d95538650afc
Venomous <stefvandenbruel@outlook.com>
2020-09-25 10:46:06 +0200

    Issue #41: CompositeSettingsDictionary.AsString() failes when the dictionary is empty
    Move to correct tests file
    
    
    method name cleanup

---
+++ Fig.Core/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fig
+{
+    public static class EnumerableExtensions
+    {
+        public static int MaxOrDefault<T>(this IEnumerable<T> enumeration, Func<T, int> selector)
+        {
+            return enumeration.Any() ? enumeration.Max(selector) : default(int);
+        }
+    }
+}
