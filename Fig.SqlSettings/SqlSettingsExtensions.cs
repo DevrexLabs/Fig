29cf566d29633957498199a04a9668c812a8ab0a
Venomous <stefvandenbruel@outlook.com>
2020-10-06 13:43:15 +0200

    Add UseSql extension + add more test coverage

---
+++ Fig.SqlSettings/SqlSettingsExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+namespace Fig.SqlSettings
+{
+    public static class SqlSettingsExtensions
+    {
+        /// <summary>
+        /// Provides a way to retrieve settings through an sql connection.
+        /// </summary>
+        /// <param name="settingsBuilder"></param>
+        /// <param name="dbConnection"></param>
+        /// <param name="dbCommand"></param>
+        /// <param name="connectionStringKey"></param>
+        /// <returns></returns>
+        public static SettingsBuilder UseSql(this SettingsBuilder settingsBuilder, IDbConnection dbConnection, 
+            string query = null, string connectionStringKey = null)
+        {
+            if (connectionStringKey != null)
+            {
+                var settings = settingsBuilder.Build();
+                var connectionString = settings.Get(connectionStringKey);
+                dbConnection.ConnectionString = connectionString ?? 
+                    throw new Exception($"ConnectionString with key '{connectionString}' was not found in the settings.");
+            }
+
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = query ?? "SELECT key,value FROM FigSettings";
+            var settingsDictionary = new SqlSettingsSource(dbConnection, dbCommand).ToSettingsDictionary();           
+            return settingsBuilder.UseSettingsDictionary(settingsDictionary);
+        }
+    }
+}
